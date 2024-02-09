4e2e46c59a63126a2ee82eb2e7b7d89ebafa555d
gargigoel09 <gargigoel858@gmail.com>
2024-02-09 13:32:54 +0530

    Tried adding http client but nto able to work around angular 17

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -4,13 +4,21 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, FormsModule, MessagesComponent, RouterOutlet, RouterLink, NgFor, NgIf],
+  imports: [CommonModule, RouterOutlet, HeroesComponent, FormsModule, MessagesComponent, RouterOutlet,
+    RouterLink, NgFor, NgIf, HttpClientModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
+  ]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
